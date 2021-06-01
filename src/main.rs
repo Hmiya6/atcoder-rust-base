b30170d60dd8dfaf46f5baeeb1e1f1198936c598
hmiya <h.miya.work@gmail.com>
2021-06-01 13:42:45 +0900

    Update template

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 // -*- coding:utf-8-unix -*-
 // Problem: 
 // Source URL: 
+//
+// 
+// Limitation:
+//
+// 
+// Memo:
+//
+//
 
 use proconio::input;
 
 fn main() {
     input! {
         n: usize,
-        mut plan: [(i32, i32, i32); n],  // Vec<(i32, i32, i32)>
+        mut a: [(i32, i32, i32); n],
     }
 }
 
